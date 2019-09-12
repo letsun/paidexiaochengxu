93484b3d7f10ca0510261a961b8f043c155b05ea
Monkey-qiang <15172750633@163.com>
2019-09-12 17:00:54 +0800

    司机页面制作修改

---
+++ pages/siji/queueRecord/queueRecord.js
@@ -0,0 +1,31 @@
+// pages/siji/queueRecord/queueRecord.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    currentData: 0,
+  },
+
+  //获取当前滑块的index
+  bindchange: function (e) {
+    const that = this;
+    that.setData({
+      currentData: e.detail.current
+    })
+  },
+  //点击切换，滑块index赋值
+  checkCurrent: function (e) {
+    const that = this;
+
+    if (that.data.currentData === e.target.dataset.current) {
+      return false;
+    } else {
+
+      that.setData({
+        currentData: e.target.dataset.current
+      })
+    }
+  }
+})
