90949236f306f2053d6e5a4819bd0b2a66cb1b6e
liang95ya <321824549@qq.com>
2019-09-16 14:34:51 +0800

    合并

---
+++ pages/siji/currentQueueDetailForm/currentQueueDetailForm.js
@@ -0,0 +1,26 @@
+// pages/siji/currentQueueDetailForm/currentQueueDetailForm.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  showModal: function() {
+    wx.showModal({
+      title: '温馨提示',
+      content: '货物已顺利送达',
+      confirmText: '确认',
+      confirmColor: '#ff8500',
+      success(res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  }
+
+})
---
+++ pages/siji/warehousing/warehousing.js
@@ -0,0 +1,16 @@
+// pages/siji/warehousing/warehousing.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // isShow: false
+  },
+  // closeCover: function(){
+  //   this.setData({
+  //     isShow: false
+  //   })
+  // }
+
+})
