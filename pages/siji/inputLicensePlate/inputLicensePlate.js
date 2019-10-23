5047363e795a52b179377efab38e07193740e2a6
Monkey-qiang <15172750633@163.com>
2019-10-23 11:35:39 +0800

    输入车牌号自动聚焦下一个输入框
    输入车牌号自动聚焦下一个输入框

--- pages/siji/inputLicensePlate/inputLicensePlate.js
+++ pages/siji/inputLicensePlate/inputLicensePlate.js
@@ -16,62 +16,113 @@ Page({
     plateNum6: '',
     plateNum7: '',
     plateNum8: '',
+
+    focus2: true,
+    focus2: false,
+    focus3: false,
+    focus4: false,
+    focus5: false,
+    focus6: false,
+    focus7: false,
+    focus8: false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
   //输入的每格车牌号
   plateNum1: function(e){
     this.setData({
       plateNum1: e.detail.value
     })
-    console.log(this.data.plateNum1)
+    //console.log(this.data.plateNum1)
+    var plateNum1 = this.data.plateNum1;
+    if (plateNum1.length == 1) {
+      this.setData({
+        focus2: true
+      })
+    }
   },
   plateNum2: function (e) {
     this.setData({
       plateNum2: e.detail.value
     })
-    console.log(this.data.plateNum2)
+    //console.log(this.data.plateNum2)
+    var plateNum2 = this.data.plateNum2;
+    if (plateNum2.length == 1) {
+      this.setData({
+        focus3: true
+      })
+    }
   },
   plateNum3: function (e) {
     this.setData({
       plateNum3: e.detail.value
     })
-    console.log(this.data.plateNum3)
+    //console.log(this.data.plateNum3)
+    var plateNum3 = this.data.plateNum3;
+    if (plateNum3.length == 1) {
+      this.setData({
+        focus4: true
+      })
+    }
   },
   plateNum4: function (e) {
     this.setData({
       plateNum4: e.detail.value
     })
-    console.log(this.data.plateNum4)
+    //console.log(this.data.plateNum4)
+    var plateNum4 = this.data.plateNum4;
+    if (plateNum4.length == 1) {
+      this.setData({
+        focus5: true
+      })
+    }
   },
   plateNum5: function (e) {
     this.setData({
       plateNum5: e.detail.value
     })
-    console.log(this.data.plateNum5)
+    //console.log(this.data.plateNum5)
+    var plateNum5 = this.data.plateNum5;
+    if (plateNum5.length == 1) {
+      this.setData({
+        focus6: true
+      })
+    }
   },
   plateNum6: function (e) {
     this.setData({
       plateNum6: e.detail.value
     })
-    console.log(this.data.plateNum6)
+    //console.log(this.data.plateNum6)
+    var plateNum6 = this.data.plateNum6;
+    if (plateNum6.length == 1) {
+      this.setData({
+        focus7: true
+      })
+    }
   },
   plateNum7: function (e) {
     this.setData({
       plateNum7: e.detail.value
     })
-    console.log(this.data.plateNum7)
+    //console.log(this.data.plateNum7)
+    var plateNum7 = this.data.plateNum7;
+    if (plateNum7.length == 1) {
+      this.setData({
+        focus8: true
+      })
+    }
   },
   plateNum8: function (e) {
     this.setData({
       plateNum8: e.detail.value
     })
-    console.log(this.data.plateNum8)
+    //console.log(this.data.plateNum8)
   },
   //点击开始排队按钮
   beginRank: function(){
@@ -88,7 +139,12 @@ Page({
     }
 
     wx.request({
-      url: api.api.getOpenId,
+      url: api.siji.submitPlateNo,
+      data: {
+        code: '1',   //司机扫描门卫出示的二维码code
+        openid: '',  //司机的openid
+        plateNo: this.data.carPlate  //司机输入的车牌号
+      },
       success: function(res){
         
       }
--- pages/siji/warehousing/warehousing.js
+++ pages/siji/warehousing/warehousing.js
@@ -1,3 +1,5 @@
+//引入接口页面
+var api = require("../../../utils/api.js");
 // pages/siji/warehousing/warehousing.js
 Page({
 
@@ -6,6 +8,24 @@ Page({
    */
   data: {
     // isShow: false
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+    //获取入库【或出库】排队单
+    // wx.request({
+    //   url: api.siji.getQuequeRecord,
+    //   data: {
+    //     codeType: 1,   //排队类型,1：入库 2：出库
+    //     driverId: '5' //driverId,string	司机id
+    //   },
+    //   success: function (res) {
+
+    //   }
+    // })
+
   },
   // closeCover: function(){
   //   this.setData({
